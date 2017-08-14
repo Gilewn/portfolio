9891a240135bdf2601b3cd53d0e857fe93a4f890
Gilewn <kolovos.giwrgos@gmail.com>
2017-08-14 05:12:42 +0300

    About and Contact Page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'pages/about' => 'pages#about', as: :about
 
+  get 'pages/contact' => 'pages#contact', as: :contact
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
